c193f54b495ec28748f1700363d8813593903fe1
neco kriel <necokriel@gmail.com>
2022-08-17 10:32:25 +1000

    simplified program names

--- search_criteria.py
+++ search_criteria.py
@@ -10,23 +10,6 @@ import datetime as dt
 
 os.system("clear")
 
-''' arXiv result properties:
-  - entry_id:         A url http://arxiv.org/abs/{id}.
-  - pdf_url:          A URL for the result's PDF if present.
-  - doi:              A URL for the resolved DOI to an external resource if present.
-  - published:        When the result was originally published.
-  - updated:          When the result was last updated.
-  - primary_category: The result's primary arXiv category.
-  - categories:       All of the result's categories.
-  - title:            The title of the result.
-  - authors:          The result's authors, as arxiv.Authors.
-  - summary:          The result abstract.
-  - comment:          The authors' comment if present.
-  - journal_ref:      A journal reference if present.
-  - links:            Up to three URLs associated with this result, as arxiv.Links.
-'''
-
-
 ## ###############################################################
 ## HELPER FUNCTIONS
 ## ###############################################################
@@ -84,10 +67,10 @@ def anyKeywordsInPhrase(phrase, list_search_terms):
       )
   return any(list_bools)
 
-def printArticleInfo(article, bool_verbose=False):
+def printArticleInfo(article):
   ## helper print function
   def printLine(name, content):
-    print(f"{name.ljust(20)} {content}\n")
+    print(f"{name.ljust(20)} {content}")
   ## remove primary category from list of other categories
   list_other_categories = [
     elem for elem in article.categories
@@ -97,9 +80,10 @@ def printArticleInfo(article, bool_verbose=False):
   ## print article information
   printLine("arXiv URL:",        article.entry_id)
   printLine("Title:",            article.title)
-  if bool_verbose: printLine("Date published:",   article.published)
-  if bool_verbose: printLine("Primary category:", article.primary_category)
-  if bool_verbose: printLine("Other categories:", ", ".join( list_other_categories ))
+  printLine("Date published:",   article.published)
+  if BOOL_PRINT_VERBOSE: printLine("Date updated:",     article.updated)
+  if BOOL_PRINT_VERBOSE: printLine("Primary category:", article.primary_category)
+  if BOOL_PRINT_VERBOSE: printLine("Other categories:", ", ".join( list_other_categories ))
   printLine("Author(s)",         ", ".join( list_authors ))
   print(" ")
 
@@ -128,98 +112,92 @@ def saveArticleInfo(file, article):
 
 
 ## ###############################################################
-## MAIN PROGRAM
+## PROGRAM PARAMETERS
 ## ###############################################################
-DATE_START           = getDateNDaysAgo(31 * 6) # approx 6 months ago
+DATE_START           = getDateNDaysAgo(31 * 12 * 3)
 DATE_FINAL           = getDateToday()
+FILENAME             = f"arxiv {DATE_FINAL} to {DATE_START}.txt"
 NUM_ARTICLES         = float(np.inf)
 BOOL_PRINT           = 1
-BOOL_PRINT_VERBOSE   = 1
+BOOL_PRINT_VERBOSE   = 0
 BOOL_SAVE            = 1
 BOOL_SEARCH_TITLE    = 1
-BOOL_SEARCH_ABSTRACT = 0
+BOOL_SEARCH_ABSTRACT = 1
 BOOL_SEARCH_AUTHORS  = 0
 LIST_CATEGORIES = [
-  # "astro-ph.HE", "astro-ph.GA", "astro-ph.SR", # astrophysics
-  # "astro-ph.IM",                               # method papers
-  "physics.flu-dyn", "physics.plasm-ph",       # fluid physics
+  ## astrophysics
+  "astro-ph.GA",
+  # "astro-ph.HE", "astro-ph.SR",
+  
+  ## method papers
+  # "astro-ph.IM",
+  
+  ## fluid physics
+  # "physics.flu-dyn", "physics.plasm-ph",
 ]
 LIST_AUTHORS = [
-  "schekochihin", "federrath", "krumholz", "beattie", "seta", "sampson"
+  "krumholz", "federrath",
+  "schekochihin", "beattie", "seta",
+  "sampson"
 ]
 LIST_KEYWORDS = [
   # "turbulen", # covers: "turbulence", "turbulent"
   # "magneti",  # covers: "magnetic", "magnetism", "magnetized"
-  "dynamo",
-  "magnetohydrodynamic", "mhd",
-  "galactic wind"
-]
-LIST_KEYWORDS_GROUPED = [
-  [ "cloud", [ "survival", "wind", "shock", "launch"] ],
+  # [ "dynamo", [ "turbulent", "fluctuation", "small" ] ],
+  # "magnetohydrodynamic", "mhd",
+  "galactic wind",
+  [ "cloud", [ "survival", "wind", "shock", "launch" ] ],
   [ "starburst", "outflows" ]
 ]
 
+
+## ###############################################################
+## MAIN PROGRAM
+## ###############################################################
 def main():
-  time_start = time.time()
-  ## initialise list of articles
-  list_articles = []
-  ## search different categories
+  time_start        = time.time()
+  list_articles     = []
+  small_fast_client = arxiv.Client(
+    page_size     = 500,
+    delay_seconds = 2,
+    num_retries   = 5
+  )
+  ## look at articles published under different categories
   for search_category in LIST_CATEGORIES:
     print("Searching:", search_category)
-    ## create search generator to fetch all available articles in category
-    search = arxiv.Search(
-      query       = search_category,
-      max_results = NUM_ARTICLES,
-      sort_by     = arxiv.SortCriterion.SubmittedDate
-    )
     ## loop through articles
-    for article in search.results():
-      ## check if the article was published within the desired date-range
+    for article in small_fast_client.results(
+        arxiv.Search(
+          query       = search_category,
+          max_results = NUM_ARTICLES,
+          sort_by     = arxiv.SortCriterion.SubmittedDate
+        )
+      ):
+      ## check that the article was published within the desired date-range
       date_published = getArticleDate(article)
       if (DATE_START <= date_published) and (date_published <= DATE_FINAL):
-        ## don't look at the article if it has appeared before
-        if any(
-            stored_article.title == article.title
-            for stored_article in list_articles
-          ):
-          continue
-        ## check if one of the keywords appears in the title / abstract
-        bool_keywords_title = BOOL_SEARCH_TITLE and any(
-          keyword in article.title.lower()
-          for keyword in LIST_KEYWORDS
-        )
-        bool_keywords_abstract = BOOL_SEARCH_ABSTRACT and any(
-          keyword in article.summary.lower()
-          for keyword in LIST_KEYWORDS
-        )
-        ## check if one of the authors appear on the article
-        list_article_authors = [
-          str(author).lower().split(" ")[-1] # grab the author's last name
-          for author in article.authors
-        ]
+        ## don't look at articles again if they have appeared before
+        if any((stored_article.title == article.title) for stored_article in list_articles): continue
+        bool_keywords_title    = BOOL_SEARCH_TITLE and anyKeywordsInPhrase(article.title.lower(), LIST_KEYWORDS)
+        bool_keywords_abstract = BOOL_SEARCH_ABSTRACT and anyKeywordsInPhrase(article.summary.lower(), LIST_KEYWORDS)
+        list_article_author_lastname = [ str(author).lower().split(" ")[-1] for author in article.authors ]
         bool_authors = BOOL_SEARCH_AUTHORS and any(
-          author in list_article_authors
-          for author in LIST_AUTHORS
+          (author_lastname in list_article_author_lastname) for author_lastname in LIST_AUTHORS
         )
-        ## if any of the criteria are satisfied
         if (bool_keywords_title or bool_keywords_abstract) or bool_authors:
           list_articles.append(article)
-      else: break # break out of loop: because articles are ordered by date
+      else: break
   print(" ")
   if BOOL_PRINT:
     print(f"{len(list_articles)} articles found:")
-    ## print articles to terminal
     for article_index, article in enumerate(list_articles):
       print(f"({article_index+1})")
-      printArticleInfo(article, BOOL_PRINT_VERBOSE)
+      printArticleInfo(article)
   if BOOL_SAVE:
-    ## create output directory
     filepath_base = os.path.dirname(os.path.realpath(__file__))
-    filename_file = f"arxiv {DATE_FINAL} to {DATE_START} (test).txt"
     filepath_output_folder = f"{filepath_base}/output"
-    filepath_output_file   = f"{filepath_output_folder}/{filename_file}"
+    filepath_output_file   = f"{filepath_output_folder}/{FILENAME}"
     createFolder(filepath_output_folder)
-    ## save articles to file
     with open(filepath_output_file, "w") as txt_file:
       for article_index, article in enumerate(list_articles):
         txt_file.write(f"({article_index+1})\n")
@@ -227,7 +205,6 @@ def main():
     print("Saved:", filepath_output_file)
     time_elapsed = time.time() - time_start
     print(f"Elapsed time: {time_elapsed:.2f} seconds.")
-
 
 
 ## ###############################################################
--- search_latest.py
+++ search_latest.py
@@ -3,45 +3,28 @@
 ## arXiv scraper: search for latest in category
 ## Author: Neco Kriel 2022
 
-import arxiv
-
-''' arXiv result properties:
-  - entry_id:         A url http://arxiv.org/abs/{id}.
-  - pdf_url:          A URL for the result's PDF if present.
-  - doi:              A URL for the resolved DOI to an external resource if present.
-  - published:        When the result was originally published.
-  - updated:          When the result was last updated.
-  - primary_category: The result's primary arXiv category.
-  - categories:       All of the result's categories.
-  - title:            The title of the result.
-  - authors:          The result's authors, as arxiv.Authors.
-  - summary:          The result abstract.
-  - comment:          The authors' comment if present.
-  - journal_ref:      A journal reference if present.
-  - links:            Up to three URLs associated with this result, as arxiv.Links.
-'''
+import os, arxiv
+os.system("clear")
 
 
 ## ###############################################################
 ## HELPER FUNCTIONS
 ## ###############################################################
 def shortenList(lst, max_num_elems=5):
-  lst_sub_step = [
+  list_sub_step = [
     str(item) if index < (max_num_elems+1)
     else "..." if index == (max_num_elems+1)
     else None
     for index, item in enumerate(lst)
   ]
   return [
-    item
-    for item in lst_sub_step
+    item for item in list_sub_step
     if item is not None
   ]
 
 def printArticleInfo(article, num_pad_chars=20):
   list_other_categories = [
-    elem
-    for elem in shortenList(article.categories)
+    elem for elem in shortenList(article.categories)
     if (elem != article.primary_category)
   ]
   print("arXiv URL:".ljust(num_pad_chars),        article.entry_id)
@@ -51,8 +34,8 @@ def printArticleInfo(article, num_pad_chars=20):
   print("Date published:".ljust(num_pad_chars),   article.published)
   if BOOL_VERBOSE:
     print("Date updated:".ljust(num_pad_chars),   article.updated)
-  print("Primary category:".ljust(num_pad_chars), article.primary_category)
-  print("Other categories:".ljust(num_pad_chars), ", ".join( list_other_categories) )
+    print("Primary category:".ljust(num_pad_chars), article.primary_category)
+    print("Other categories:".ljust(num_pad_chars), ", ".join( list_other_categories) )
   print("Author(s)".ljust(num_pad_chars),         ", ".join( shortenList(article.authors)) )
   if BOOL_VERBOSE:
     print("Abstract:")
@@ -63,10 +46,9 @@ def printArticleInfo(article, num_pad_chars=20):
 ## ###############################################################
 ## MAIN PROGRAM
 ## ###############################################################
-BOOL_VERBOSE = 1
-SEARCH_TERM  = "physics.pop-ph" # science communication
-# SEARCH_TERM  = "magneti"
-NUM_ARTICLES = 50
+BOOL_VERBOSE = 0
+SEARCH_TERM  = "dynamo"
+NUM_ARTICLES = 250
 
 def main():
   search = arxiv.Search(
