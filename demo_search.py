21ba4716296a4032f7bfa7fce8b2177bb4deaeca
neco kriel <necokriel@gmail.com>
2022-08-17 10:31:26 +1000

    dev ability to search for multiple search criteria

--- demo_search.py
+++ demo_search.py
@@ -6,7 +6,6 @@ def printHeading(str):
   print(str)
   print("=" * len(str))
 
-
 def allKeywordsInPhrase(phrase, list_search_terms):
   list_bools = []
   for term in list_search_terms:
@@ -18,7 +17,6 @@ def allKeywordsInPhrase(phrase, list_search_terms):
         anyKeywordsInPhrase(phrase, term)
       )
   return all(list_bools)
-
 
 def anyKeywordsInPhrase(phrase, list_search_terms):
   list_bools = []
@@ -33,11 +31,10 @@ def anyKeywordsInPhrase(phrase, list_search_terms):
       )
   return any(list_bools)
 
-
 def main():
-  list_animals    = [ "fox", "lion", "hippo" ]
-  list_actions    = [ "jumped", "climbed" ]
-  list_objects    = [ "hill", "log", "turtle" ]
+  list_animals    = [ "fox", "turtle", "cow" ]
+  list_actions    = [ "jumped", "leaped" ]
+  list_objects    = [ "moon", "log", "river" ]
   list_phrases    = []
   for animal in list_animals:
     for action in list_actions:
@@ -47,18 +44,16 @@ def main():
   print("\n".join(list_phrases))
   print(" ")
   list_search_terms = [
-    # "fox",
     # "turtle",
-    # [ "lion", "turtle" ], # fox and turtle
-    # [ "fox", ["jumped", "turtle"] ], # fox and (jumped or turtle)
-    [ ["fox", "hippo"], "jumped", ["log", "turtle"] ],
+    # [ "cow", "river" ], # cow and river
+    # [ "fox", ["jumped", "river"] ], # fox and (jumped or river)
+    [ ["fox", "cow"], "jumped", ["moon", "log"] ], # (fox or cow) and jumped and (moon or log)
   ]
   printHeading("List of phrases that met condition:")
   for phrase in list_phrases:
     if anyKeywordsInPhrase(phrase, list_search_terms):
       print(phrase)
       continue
-
 
 if __name__ == "__main__":
   main()
