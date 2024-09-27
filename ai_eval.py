08d193c66a7044cb2985764f0d10829833b2314d
Cameron <CameronJonesAustralia@gmail.com>
2024-09-27 10:56:49 +1000

    idk

--- ai_eval.py
+++ ai_eval.py
@@ -5,6 +5,14 @@ from openai import OpenAI
 import json
 
 
+# This is the LLM model to use. Make sure it is installed on your system
+model = "llama3:latest"
+# model = "phi3.5:latest"    
+# model = "mistral-nemo"
+
+configPath = "./Configs/FSOC.json" # This will need to change
+
+
 
 def load_prompts(json_path):
     """ 
