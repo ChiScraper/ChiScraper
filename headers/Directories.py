ad37136b7f71a4f86f735ecda588cd5a375f9b92
Cameron Jones <cameronjonesaustralia@gmail.com>
2024-10-29 15:03:30 +1100

    Fixed relative pathing

--- headers/Directories.py
+++ headers/Directories.py
@@ -1,3 +1,9 @@
-directory_config  = "./configs"
-directory_mdfiles = "./mdfiles"
-directory_pdfs    = "./pdfs"
+import os
+# The root is the parent directory of the current file
+directory_root = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+print(f"Root directory: {directory_root}")
+
+
+directory_config  = os.path.join(directory_root, "configs")
+directory_mdfiles = os.path.join(directory_root, "mdfiles")
+directory_pdfs    = os.path.join(directory_root, "pdfs")
