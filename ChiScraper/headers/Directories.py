42e65c4abe9c154d5d520ec30914bcbb00152cc4
Cameron Jones <cameronjonesaustralia@gmail.com>
2024-10-29 12:43:39 +1100

    I think the package works propper now

--- ChiScraper/headers/Directories.py
+++ ChiScraper/headers/Directories.py
@@ -1,3 +1,19 @@
-directory_config  = "./configs"
-directory_mdfiles = "./mdfiles"
-directory_pdfs    = "./pdfs"
+config  = "configs"
+mdfiles = "mdfiles"
+pdfs    = "pdfs"
+
+
+import os
+# This grabs the root directory of the project. I.e., one level above the headers directory.
+root_directory = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+# Construct the full path to each directory
+
+directory_config  = os.path.join(root_directory, config)
+directory_mdfiles = os.path.join(root_directory, mdfiles)
+directory_pdfs    = os.path.join(root_directory, pdfs)
+
+print(f"Root directory: {root_directory}")
+print(f"Config directory: {directory_config}")
+print(f"Markdown files directory: {directory_mdfiles}")
+print(f"PDFs directory: {directory_pdfs}")
