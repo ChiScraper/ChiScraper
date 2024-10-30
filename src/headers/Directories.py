9d1f5cad6b7c2adf424d5da85499dc86b2c93e60
Cameron Jones <cameronjonesaustralia@gmail.com>
2024-10-30 17:29:14 +1100

    added relative pathing to directories

--- src/headers/Directories.py
+++ src/headers/Directories.py
@@ -1,3 +1,8 @@
-directory_config  = "./configs"
-directory_mdfiles = "./mdfiles"
-directory_pdfs    = "./pdfs"
+import os
+from pathlib import Path
+
+directory_root = Path(__file__).resolve().parents[2]
+directory_config  = os.path.join(directory_root, "configs")
+directory_mdfiles = os.path.join(directory_root, "mdfiles")
+directory_pdfs    = os.path.join(directory_root, "pdfs")
+
