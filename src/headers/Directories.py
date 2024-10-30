07c505fcef4645b0452fe5064ee1b46b90bc8130
Cameron Jones <cameronjonesaustralia@gmail.com>
2024-10-30 17:33:38 +1100

    Adding back in the webapp config files

--- src/headers/Directories.py
+++ src/headers/Directories.py
@@ -1,20 +1,9 @@
 import os
-<<<<<<< HEAD
-# The root is the parent directory of the current file
-directory_root = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-print(f"Root directory: {directory_root}")
-
-
-directory_config  = os.path.join(directory_root, "configs")
-directory_mdfiles = os.path.join(directory_root, "mdfiles")
-directory_pdfs    = os.path.join(directory_root, "pdfs")
-directory_webApp  = os.path.join(directory_root,"scripts", "WebAppConfig")
-=======
 from pathlib import Path
 
 directory_root = Path(__file__).resolve().parents[2]
 directory_config  = os.path.join(directory_root, "configs")
 directory_mdfiles = os.path.join(directory_root, "mdfiles")
 directory_pdfs    = os.path.join(directory_root, "pdfs")
+directory_webApp  = os.path.join(directory_root,"scripts", "WebAppConfig")
 
->>>>>>> master
